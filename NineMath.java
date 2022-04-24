46f27db39203e322be97d371350bfe1c37c751c6
BrnBrian <brianelicc@gmail.com>
2022-04-24 17:47:34 -0400

    Exercise optimal

---
+++ NineMath.java
@@ -0,0 +1,28 @@
+
+
+
+
+
+/**
+
+Author: Brnnnn
+Version: JDK8.0
+
+
+*/
+
+
+
+//9*9 exe:
+class  NineMath{
+	public static void main(String[] args) {
+
+		for (int i = 1;i < 10 ;i++){
+			for (int j = 1;j <=i ;j++){
+				System.out.print(i + "*" + j + "=" + (i*j) + " ");
+			}
+			System.out.println();
+		}
+		
+	}
+}
